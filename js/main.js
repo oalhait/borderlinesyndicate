ad1de641909529d15ef997a80bee6b3621ad5175
Wolf <obedappiah-agyeman@Obeds-MacBook-Pro.local>
2018-06-03 15:24:56 -0700

    new changes

--- js/main.js
+++ js/main.js
@@ -6,9 +6,6 @@
 // <img id="book" src="book.png" alt="" width="100" height="123">
  
 // With the element initially hidden, we can show it slowly:
-$("#container").fadeIn( "slow", function() {
-    
-  });
 
 
   $(document).ready(function(){
